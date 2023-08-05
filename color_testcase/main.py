0358d6eee16161a81b157b380afc9c3bf7257744
Bluemi <bruno.schilling@protonmail.ch>
2023-08-05 19:12:23 +0200

    add black bodies; add color_system; add first spectrum plot

--- color_testcase/main.py
+++ color_testcase/main.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+from color_system import cs_hdtv
+from black_bodies import planck
+
+
 def main():
-    print('hello world!')
+    print('main')
+    cs = cs_hdtv
+    lam = np.arange(380., 781., 5)
+    spec = planck(lam, 6000)
+
+    plt.plot(lam, spec)
+    plt.show()
+
+    print(np.max(spec))
 
 
 if __name__ == '__main__':
     main()
-
--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-python3 color_testcase/main.py
+case "$1" in 
+	b)
+		python3 color_testcase/black_bodies.py
+		;;
+	*)
+		python3 color_testcase/main.py
+		;;
+esac
